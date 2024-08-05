77389cc80618c917623b5707a520d234abadde59
Xuân Khu Bùi <14572502-xuankhubui@users.noreply.gitlab.com>
2024-08-05 21:56:50 +0700

    initial server with port 8080

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log("test");
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const cors = require('cors');
+// CROS middleware 
+/*
+app.use(function(req, res, next) {
+    // Mọi domain
+    res.header("Access-Control-Allow-Origin", "*");
+   
+    // Domain nhất định
+    // res.header("Access-Control-Allow-Origin", "https://freetuts.net");
+   
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+*/
+const cookieParser = require('cookie-parser');
+const db = require('./models');
+const bcrypt = require('bcrypt');
+const router = require('./routers');
+dotenv.config();
+
+app.listen(8080,"127.0.0.1", () => {
+    console.log(`Server is running on port`);
+} )
